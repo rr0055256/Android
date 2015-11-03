3345a97bbf97603783398f5b0089b2d3916687fb
rr0055256 <raph_rahul@yahoo.co.in>
2015-11-03 23:54:39 +0530

    NavDrawerIntermediateCommit

--- NavigationDrawer/app/src/main/java/com/helloworld/raman/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/helloworld/raman/navigationdrawer/MainActivity.java
@@ -1,15 +1,29 @@
 package com.helloworld.raman.navigationdrawer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] cricketer_names;
+    private DrawerLayout cDrawerLayout;
+    private ListView cricketerList;
+    private String[] cricketerImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,66 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //retrieve values from the strings xml
+        cricketer_names = getResources().getStringArray(R.array.cricketer_names);
+
+        cricketerImages = getResources().getStringArray(R.array.cricketer_images);
+
+        //define DrawerLayout Object
+        cDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //object of listview
+        cricketerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        CustomDrawer drawer = new CustomDrawer(this);
+
+        cricketerList.setAdapter(drawer);
+
+        cricketerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+
+    }
+
+    class CustomDrawer extends ArrayAdapter<String>{
+
+        public CustomDrawer(Context context) {
+            super(context, 0);
+        }
+
+        @Override
+        public int getPosition(String item) {
+            return super.getPosition(item);
+        }
+
+        @Override
+        public int getCount() {
+            return super.getCount();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView ==null) {
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+
+                convertView = inflater.inflate(R.layout.drawer_list_item, null, true);
+            }
+            ImageView cricketerImage = (ImageView) convertView.findViewById(R.id.cricketerImage);
+
+            TextView cricTextView = (TextView) convertView.findViewById(R.id.cricketerName);
+
+            cricTextView.setText(cricketer_names[position]);
+
+            cricketerImage.setImageResource(getPosition(cricketerImages[position]));
+
+            return convertView;
+        }
     }
 
     @Override
